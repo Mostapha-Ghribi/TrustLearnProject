c7862f313deb1dc50c5f78121d3811d83b3e319f
Mostapha Ghribi <mostapha.ghribi20@gmail.com>
2021-11-22 18:50:39 +0100

    some changes

--- server/index.js
+++ server/index.js
@@ -11,7 +11,11 @@ const app = express();
 dotenv.config();
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors());
+app.use(cors(
+  {
+    origin: ['http://localhost:4200']
+}
+));
 app.use(userRoutes);
 const PORT = process.env.PORT|| 5000;
 app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}...`))
